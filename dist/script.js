9250099e5fcd3629a9d32e04ce9f0a06102dd3c4
sudeepk <sudeepk@ccs.neu.edu>
2017-10-18 13:30:04 -0400

    Added new data sources

--- dist/script.js
+++ dist/script.js
@@ -3,29 +3,36 @@
 (() => {
     $(document).ready(() => {
         //Load the JSON
-        $.getJSON('imdb.json', (data) => {
-            console.log(data);
+        $.getJSON('data/imdb1k.json', (data) => {
             
             //Variable declaration
             let graphGenerator, graph, layout, graphics, renderer;
             
             graphGenerator = Viva.Graph.generator();
             graph = Viva.Graph.graph();
-            
+
             //Add nodes to graph
             data.nodes.forEach((node) => graph.addNode(node.id, {name: node.name, type: node.type}));
             //Add edges to graph
             data.edges.forEach((edge) => graph.addLink(edge.source, edge.target, edge.value));
-            
+            console.log(data.nodes.length, data.edges.length);
             graphics = Viva.Graph.View.webglGraphics();
-            
+
+            layout = Viva.Graph.Layout.forceDirected(graph, {
+                springLength : 30,
+                springCoeff : 0.0001,
+                dragCoeff : 0.009,
+                gravity : -1.2
+            });
+
             renderer = Viva.Graph.View.renderer(graph, {
                 graphics,
+                layout,
                 renderLinks: true
             });
             
             renderer.run();
-        })
+        });
     }); 
     
     
