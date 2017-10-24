81b774c72657677eb25b59e2f4766a539c9d24eb
Bingyu Wang <rainicy925@gmail.com>
2017-10-23 20:17:26 -0400

    zoom in and out.

--- dist/script.js
+++ dist/script.js
@@ -5,17 +5,18 @@
         //Load the JSON
         let container = document.body;
 
-        $.getJSON('data/imdb1k.json', (data) => {
+        $.getJSON('data/imdb1k_p.json', (data) => {
 
             //Variable declaration
             let graphGenerator, graph, layout, graphics, renderer, domLabels;
 
             graphGenerator = Viva.Graph.generator();
             graph = Viva.Graph.graph();
-            // graph = graphGenerator.grid(1000, 1000);
+            // graph = graphGenerator.grid(480, 1);
 
             //Add nodes to graph
             data.nodes.forEach((node) => graph.addNode(node.id, {name: node.name, type: node.type}));
+            console.log("total nodes:" + data.nodes.length);
             //Add edges to graph
             data.edges.forEach((edge) => graph.addLink(edge.source, edge.target, edge.value));
 
@@ -38,7 +39,13 @@
             graphics.node((node) => {
                 let nodeColor = node.data.type === 'feature' ? '#7836CF' : '#BF0A0A';
                 let nodeSize = node.data.type === 'label' ? 50 : 10;
-                return  Viva.Graph.View.webglSquare(nodeSize, nodeColor);
+                // return  Viva.Graph.View.webglSquare(nodeSize, nodeColor);
+                var ui = Viva.Graph.View.webglSquare(nodeSize, nodeColor);
+                ui.addEventListener('click', function () {
+                    // toggle pinned mode
+                    layout.pinNode(node, !layout.isNodePinned(node));
+                });
+                return ui;
             });
 
             layout = Viva.Graph.Layout.forceDirected(graph, {
@@ -71,6 +78,7 @@
             $('#centerForm').submit(function(e) {
                 e.preventDefault();
                 var nodeId = $('#nodeid').val();
+                console.log("nodeId: " + nodeId)
                 if (graph.getNode(nodeId)) {
                     var pos = layout.getNodePosition(nodeId);
                     renderer.moveTo(pos.x, pos.y);
@@ -84,12 +92,14 @@
 
             var prevSelected;
             function highlightNode(nodeId) {
-                var ui = graphics.getNodeUI(nodeId);
+                var ui = graphics1.getNodeUI(nodeId);
                 if (prevSelected) {
-                    prevSelected.attr('fill', '#BF0A0A')
+                    // prevSelected.attr('fill', '#BF0A0A')
+                    prevSelected.color = -1089860865;
                 }
                 prevSelected = ui;
-                ui.attr('fill', 'orange');
+                // ui.attr('fill', 'orange');
+                ui.color = -1039860865;
             }
 
             // listen to events
