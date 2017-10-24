ef449cbb48b2d8c1c2b2f06879e86b3549bf1ddb
Bingyu Wang <rainicy925@gmail.com>
2017-10-24 13:13:49 -0400

    positive edges added.

--- dist/script.js
+++ dist/script.js
@@ -5,7 +5,7 @@
         //Load the JSON
         let container = document.body;
 
-        $.getJSON('data/imdb5k.json', (data) => {
+        $.getJSON('data/imdb2k_p.json', (data) => {
 
             //Variable declaration
             let graphGenerator, graph, layout, graphics, renderer, domLabels;
